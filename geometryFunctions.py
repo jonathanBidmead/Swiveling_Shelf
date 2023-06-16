config, cleaning, nonzero initial rotation fix
created config files, did some cleanup + changed some variable names, added offset by rotY[0] in some places to fix non-zero initial rotations. Added new cabinet + path. Changed flipped normal threshold to work only for negative lengths

geometryFunctions.py
@@ -1,6 +1,6 @@
 import numpy as np
 from matplotlib.path import Path
-
+from config import *
 #class defining walls
 class wall:
     def __init__(self,startPoint,endPoint,type):
@@ -61,7 +61,7 @@ def lineLength(pt1,pt2):
     try:
         return np.sqrt((x2-x1)**2+(y2-y1)**2)
     except:
-        return 10
+        return MAX_TEST_VECTOR_LENGTH
     
 def lengthToOpenDoor(angle,doorStart,doorEnd,pointStart,length):
     x1 = pointStart[0]

highLevelFunctions.py
@@ -2,7 +2,7 @@ import numpy as np
 from geometryFunctions import *
 import matplotlib.pyplot as plt
 from matplotlib.path import Path
-
+from config import *
 
 def computeMinLengthForEachAngle(maxLength,angleList,lineNumPoints,lineX,lineY,vectorLength,rotY,cabinetWalls):
     minLengthList = []
@@ -103,7 +103,7 @@ def ComputeMaxLengthAlongNormal(lineX,lineY,rotY,cabinetWalls,midpt,normpt):
 
     cabinetPath = createCabinetPath(cabinetWalls)
     reversedNormalFlag = False
-    length = 10#TODO set up defined variables for all these things
+    length = MAX_TEST_VECTOR_LENGTH
     midpt[0] = midpt[0] - lineX[0]
     midpt[1] = midpt[1] - lineY[0]
     normpt[0] = normpt[0] - lineX[0]
@@ -118,12 +118,12 @@ def ComputeMaxLengthAlongNormal(lineX,lineY,rotY,cabinetWalls,midpt,normpt):
         
         
         
-        length = 10#TODO
+        length = MAX_TEST_VECTOR_LENGTH
         reversedNormalFlag = False
         #computing transformation relative to startpoint
         tx = lineX[i] - lineX[0]
         ty = lineY[i] - lineY[0]
-        theta = rotY[i]
+        theta = rotY[i] - rotY[0]
         
         #new midpt (x then y)
         pt1[0] = midpt[0]*np.cos(theta) - midpt[1]*np.sin(theta) + tx + lineX[0]
@@ -151,8 +151,6 @@ def ComputeMaxLengthAlongNormal(lineX,lineY,rotY,cabinetWalls,midpt,normpt):
                     lengthTemp = - lengthTemp
                     # print(lengthTemp,midpt[0]+lineX[0],midpt[1]+lineY[0])
                 length = lengthTemp
-                chosenWall = wall
-        wallList.append(chosenWall)
         maxLengthList.append(length)
 
 
@@ -165,8 +163,9 @@ def ComputeMaxLengthAlongNormal(lineX,lineY,rotY,cabinetWalls,midpt,normpt):
     minLength = maxLengthList[0]
     for i in range(len(maxLengthList)):
         minTemp = maxLengthList[i]
-        if minTemp < minLength and abs(minTemp) < threshold:
-            minLength = minTemp
+        if minTemp < minLength:
+            if (abs(minTemp) < threshold and minTemp < 0) or minTemp > 0:#if mintemp is below 0 but smaller than the threshold, or if it is greater than 0, it works
+                minLength = minTemp
 
 
     # minLength = maxLengthList[mindex]
@@ -178,11 +177,12 @@ def ComputeMaxLengthAlongNormal(lineX,lineY,rotY,cabinetWalls,midpt,normpt):
     normpt[0] = normpt[0] + lineX[0]
     normpt[1] = normpt[1] + lineY[0]
 
-    if abs(minLength) == 10:#TODO add to list of max lengths
+    if abs(minLength) == MAX_TEST_VECTOR_LENGTH:
         for wall in cabinetWalls:
             if wall.type == "open":
                 [Px,Py] = computeIntersection(midpt,normpt,wall.startPoint,wall.endPoint)
-                return [Px,Py]
+                if not Px == None:
+                    return [Px,Py]
 
 
     # print(min(maxLengthList),min(maxLengthList2))

pulloutShelfGenerator.py
@@ -16,54 +16,60 @@ from geometryFunctions import *
 from highLevelFunctions import *
 from matplotlib.animation import FuncAnimation
 import bisect
+from config import *
 
 #must be constructed such that startpoint is the same as the endpoint for the next vertex in the anticlockwise direction
-backwall  = wall([0,3],[5,3],"solid")
-sidewallL = wall([0,1],[0,3],"solid")
-sidewallR = wall([5,3],[5,1],"solid")
-frontwall = wall([5,1],[2.5,1],"solid")
-door      = wall([0,1],[2.5,1],"open")
-thickness1 = wall(door.startPoint,[door.startPoint[0],door.startPoint[1]-0.1],"solid")
-thickness2 = wall(door.endPoint,[door.endPoint[0],door.endPoint[1]-0.1],"solid")
+#cabinet 1
+# backwall  = wall([0,3],[5,3],"solid")
+# sidewallL = wall([0,1],[0,3],"solid")
+# sidewallR = wall([5,3],[5,1],"solid")
+# frontwall = wall([5,1],[2.5,1],"solid")
+# door      = wall([0,1],[2.5,1],"open")
+# thickness1 = wall(door.startPoint,[door.startPoint[0],door.startPoint[1]-0.1],"solid")
+# thickness2 = wall(door.endPoint,[door.endPoint[0],door.endPoint[1]-0.1],"solid")
+# pivot = sidewallL.startPoint
 
+# #will be converted to matplotlib path, which requires vertices in order.
+# #convention will be: append walls in clockwise order starting from left side of door.
+# #TODO may be wrong, but for now: leave the thickness as the last entry and do not count it for purposes of the path.
+# cabinetWalls = []
+# cabinetWalls.append(sidewallL)
+# cabinetWalls.append(backwall)
+# cabinetWalls.append(sidewallR)
+# cabinetWalls.append(frontwall)
+# cabinetWalls.append(door)
+# # cabinetWalls.append(thickness1)
+# cabinetWalls.append(thickness2)
 
-#will be converted to matplotlib path, which requires vertices in order.
-#convention will be: append walls in clockwise order starting from left side of door.
-#TODO may be wrong, but for now: leave the thickness as the last entry and do not count it for purposes of the path.
-cabinetWalls = []
-cabinetWalls.append(sidewallL)
-cabinetWalls.append(backwall)
-cabinetWalls.append(sidewallR)
-cabinetWalls.append(frontwall)
-cabinetWalls.append(door)
-# cabinetWalls.append(thickness1)
-cabinetWalls.append(thickness2)
+#CABINET 1
+# from cabinetsAndPaths import cabinet1 as cabinetWalls
+# from cabinetsAndPaths import lineX1 as lineX
+# from cabinetsAndPaths import lineY1 as lineY
+# from cabinetsAndPaths import rotX1 as rotX
+# from cabinetsAndPaths import rotY1 as rotY
 
-#fiddle-able variables
-lineNumPoints = 1000
-numAngles = 100
-maxLength = 10
-vectorLength = 10 #TODO: should be tied to dimension of cabinet ideally
-lengthThreshold = 0.05 #target length between points for 2ndary calculation
+#CABINET 2
+from cabinetsAndPaths import cabinet2 as cabinetWalls
+from cabinetsAndPaths import lineX2 as lineX
+from cabinetsAndPaths import lineY2 as lineY
+from cabinetsAndPaths import rotX2 as rotX
+from cabinetsAndPaths import rotY2 as rotY
 #step 2. Two lines, each defined by bezier curves. Line1 defines the translational component of the shelf's movement. Line2 defines the rotational component.
 
-#use bezier curve to create path for shelf to follow.
-# line1 = [3,2.2 , 1.8,1.5 , 1.8,-0.1 , 0.3,2.5]
-# [lineX,lineY] = bezier(line1,lineNumPoints)
 
-#using circular path for shelf translational motion
-theta = np.linspace(-0.4*np.pi,0.15*np.pi,lineNumPoints)
-theta = theta[::-1]#reverse the points so the start point is inside the cabinet
-lineX = 1.5*np.cos(theta)
-lineY = 1.5*np.sin(theta)+1
+# #using circular path for shelf translational motion
+# theta = np.linspace(-0.4*np.pi,0.15*np.pi,lineNumPoints)
+# theta = theta[::-1]#reverse the points so the start point is inside the cabinet
+# lineX = 1.5*np.cos(theta)+pivot[0]
+# lineY = 1.5*np.sin(theta)+pivot[1]
 
-#use another bezier to define the angle through which the shelf turns at each point along the line
-line2 = [0,0 , 0.1*lineNumPoints,50 , 0.9*lineNumPoints,20 , lineNumPoints,70]
+# #use another bezier to define the angle through which the shelf turns at each point along the line
 # line2 = [0,0 , 0.1*lineNumPoints,50 , 0.9*lineNumPoints,20 , lineNumPoints,70]
-[rotX,rotY] = bezier(line2,lineNumPoints)
-rotY = rotY*np.pi/180
-# rotY = np.zeros(lineNumPoints)
-# rotY = np.linspace(0,100*np.pi/180,lineNumPoints)#TEMP for testing
+# # line2 = [0,0 , 0.1*lineNumPoints,50 , 0.9*lineNumPoints,20 , lineNumPoints,70]
+# [rotX,rotY] = bezier(line2,lineNumPoints)
+# rotY = rotY*np.pi/180
+# # rotY = np.zeros(lineNumPoints)
+# # rotY = np.linspace(0,100*np.pi/180,lineNumPoints)#TEMP for testing
 
 
 #step 3: get vectors for each angle, traverse path, find shortest vector for each.
@@ -74,53 +80,62 @@ angleList = angleList.tolist()
 
 #for each point on the line, add a vector with appropriate length and angle. Get resultant end point to find line points. Calculate intersections with all walls. Find minimum
 
-# print(angleList*180/np.pi)
 
 print("getting max vector length for each angle")
 [minLengthList,linePosForMinL] = computeMinLengthForEachAngle(maxLength,angleList,lineNumPoints,lineX,lineY,vectorLength,rotY,cabinetWalls)
 print("done")
+
 #initiating plot
 fig, ax = plt.subplots()
 
+#find door
+door = None
+for wall in cabinetWalls:
+    if wall.type == "open":
+        door = wall
+
 print("assembling vectors around single point")
-[xFinal,yFinal] = assembleVectorsAroundPoint(lineX,lineY,numAngles,vectorLength,angleList,rotY,door,minLengthList,linePosForMinL,maxLength)
+[xShelfOutline,yShelfOutline] = assembleVectorsAroundPoint(lineX,lineY,numAngles,vectorLength,angleList,rotY,door,minLengthList,linePosForMinL,maxLength)
 print("done")
 
-print("secondary stage")
+print("Begin Midpoint-Normal Extension")
 #expanding the area in a secondary stage. Find all sides with length above threshold, get midpoint, create new point expanded along the normal
 
 allDistancesBelowThreshold = False
 distanceTooLong = False
-iterativeExtensionAlongNormal = 10
+iterativeExtensionAlongNormal = MAX_TEST_VECTOR_LENGTH
 test_iter = 0
-max_test_iter = 200#when < test_iter, nothing here is used
+max_test_iter = 100#when < test_iter, nothing here is used
 master_iters = 0
-max_master_iters = 100000
+max_master_iters = 1000
+
+# looping through all pairs of adjacent points until the distance between each pair is below the threshold.
+# if distance too large, find midpoint, get vector normal to pt1 & pt2, find nearest intersection with wall, and insert a point there into the shelf outline
 while not allDistancesBelowThreshold:
     distanceTooLong = False
     firstPointOnWall = False
     secondPointOnWall = False
     continueFlag = False
     testBool = False
-    # xFinal3 = xFinal
-    # yFinal3 = yFinal
-    for i in range(len(xFinal)):
+    
+    for i in range(len(xShelfOutline)):
 
         #making sure the thing can wrap around
-        x1 = xFinal[i]
-        y1 = yFinal[i]
+        x1 = xShelfOutline[i]
+        y1 = yShelfOutline[i]
         try:#getting next point. Wraps around when at end of the list
-            x2 = xFinal[i+1]
-            y2 = yFinal[i+1]
+            x2 = xShelfOutline[i+1]
+            y2 = yShelfOutline[i+1]
         except:
-            x2 = xFinal[0]
-            y2 = yFinal[0]
+            x2 = xShelfOutline[0]
+            y2 = yShelfOutline[0]
 
         length = lineLength([x1,y1],[x2,y2])#get length between two neighbouring points
 
 
         #edge case: if both points are on the same wall, just make a new midpt and leave it at that
-        if length > lengthThreshold:
+        # instead of trying to expand along an undefined normal vector (it could go either way)
+        if length > lengthThreshold and test_iter < max_test_iter:
             for wall in cabinetWalls:
                 if not pointsTheSame([x1,y1],[x2,y2]):
                     firstPointOnWall = isPointOnLine(wall.startPoint,wall.endPoint,[x1,y1])
@@ -128,35 +143,35 @@ while not allDistancesBelowThreshold:
                         secondPointOnWall = isPointOnLine(wall.startPoint,wall.endPoint,[x2,y2])
 
                         if secondPointOnWall:
-                            # [newX,newY] = pointOnLineAtDist([x1,y1],[x2,y2],lengthThreshold)
                             [newX,newY] = getMidPoint([x1,y1],[x2,y2])
-                            xFinal.insert(i+1,newX)
-                            yFinal.insert(i+1,newY)
-                            print("wall point inserted",newX,newY,i)
+                            if newX == None:
+                                print("whoops")
+                            xShelfOutline.insert(i+1,newX)
+                            yShelfOutline.insert(i+1,newY)
+                            # print("wall point inserted",newX,newY,i)
                             continueFlag = True
-                            # distanceTooLong = True
                             break
-        if continueFlag:
-            continue
+            if continueFlag:
+                continue
 
-        
-        if (length > lengthThreshold and (test_iter < max_test_iter)):#if length too large, get midpoint. Create new point along the normal (for now just iterate on small distance)
-            
+        #if length too large, get midpoint. Create new point along the normal at intersection with nearest wall
+        if (length > lengthThreshold and (test_iter < max_test_iter)):
             #get midpoint of offending section
             [mx,my] = getMidPoint([x1,y1],[x2,y2])
 
             #get normal to line segment at midpoint
             #normalise then scale to iterativeExtensionAlongNormal
-            # print([mx,my])
             [normX,normY] = newPointAlongNormal([x1,y1],[x2,y2],[mx,my],iterativeExtensionAlongNormal)
 
             
             #find maximum extensible length along normal before hitting a wall, generate new point along that vector.
             [newX,newY] = ComputeMaxLengthAlongNormal(lineX,lineY,rotY,cabinetWalls,[mx,my],[normX,normY])
             # ax.plot(mx,my,'x')
-            xFinal.insert(i+1,newX)
-            yFinal.insert(i+1,newY)
-            print("line point inserted",newX,newY,i)
+            xShelfOutline.insert(i+1,newX)
+            yShelfOutline.insert(i+1,newY)
+            if newX == None:
+                print("whoops2")
+            # print("line point inserted",newX,newY,i)
             # i = i + 1
             test_iter = test_iter + 1
             if testBool == True:
@@ -165,29 +180,29 @@ while not allDistancesBelowThreshold:
             testBool = True
 
 
-    for i in range(len(xFinal)):
+    for i in range(len(xShelfOutline)):
         #making sure the thing can wrap around
-        x1 = xFinal[i]
-        y1 = yFinal[i]
+        x1 = xShelfOutline[i]
+        y1 = yShelfOutline[i]
         try:#getting next point. Wraps around when at end of the list
-            x2 = xFinal[i+1]
-            y2 = yFinal[i+1]
+            x2 = xShelfOutline[i+1]
+            y2 = yShelfOutline[i+1]
         except:
-            x2 = xFinal[0]
-            y2 = yFinal[0]
+            x2 = xShelfOutline[0]
+            y2 = yShelfOutline[0]
 
         length = lineLength([x1,y1],[x2,y2])#get length between two neighbouring points
 
         if length > lengthThreshold:
             distanceTooLong = True
 
-    if distanceTooLong == False or master_iters > max_master_iters:
+    if distanceTooLong == False or master_iters >= max_master_iters or test_iter >= max_test_iter:
         allDistancesBelowThreshold = True
         if distanceTooLong == False:
             print("all distances below threshold")
-        if test_iter > max_test_iter:
+        if test_iter >= max_test_iter:
             print("iteration limit: too many points added in secondary stage")
-        if master_iters > max_master_iters:
+        if master_iters >= max_master_iters:
             print("iteration limit: master_iters too large")
     master_iters = master_iters + 1
 
@@ -195,7 +210,7 @@ print("secondary stage complete")
 
 print("begin plotting")
 #plotting from here on
-ax.plot(xFinal,yFinal,'-b')#plot the outline of the shelf
+ax.plot(xShelfOutline,yShelfOutline,'-b')#plot the outline of the shelf
 ax.plot(rotX/lineNumPoints + 3,rotY - 1)#plot the rotational curve
 ax.plot(lineX,lineY,'g:')#plot the path of travel
 
@@ -205,63 +220,63 @@ for wall in cabinetWalls:
 
 
 
-xFinal2 = []
-yFinal2 = []
-line, = ax.plot([], [],'-g', lw=3)
+xShelfOutline2 = []
+yShelfOutline2 = []
+line, = ax.plot([], [],'-gs', lw=3)
 #animating the shelf to move along the pathway
 #TODO make this work with new method
 
 #testing
-# xFinal = np.linspace(0,1,200)+2 - lineX[0]
-# yFinal = 1*np.sin(xFinal)+2 - lineY[0]
-xFinal = xFinal - lineX[0]
-yFinal = yFinal - lineY[0]
+# xShelfOutline = np.linspace(0,1,200)+2 - lineX[0]
+# yShelfOutline = 1*np.sin(xShelfOutline)+2 - lineY[0]
+xShelfOutline = xShelfOutline - lineX[0]
+yShelfOutline = yShelfOutline - lineY[0]
 
 def init():
     line.set_data([], [])
     return line,
 def animate(i):
-    xFinal2 = []
-    yFinal2 = []
+    xShelfOutline2 = []
+    yShelfOutline2 = []
     #for each point in the line, redraw the whole shelf
     
-    for j in range(len(xFinal)):
+    for j in range(len(xShelfOutline)):
         tx = lineX[i] - lineX[0]
         ty = lineY[i] - lineY[0]
-        theta = rotY[i]
-        # xFinal[j] = xFinal[j] - lineX[0]
-        # yFinal[j] = yFinal[j] - lineY[0]
+        theta = rotY[i] - rotY[0]
+        # xShelfOutline[j] = xShelfOutline[j] - lineX[0]
+        # yShelfOutline[j] = yShelfOutline[j] - lineY[0]
 
-        xFinal2.append(xFinal[j]*np.cos(theta) - yFinal[j]*np.sin(theta)+tx+lineX[0])
-        yFinal2.append(xFinal[j]*np.sin(theta) + yFinal[j]*np.cos(theta)+ty+lineY[0])
+        xShelfOutline2.append(xShelfOutline[j]*np.cos(theta) - yShelfOutline[j]*np.sin(theta)+tx+lineX[0])
+        yShelfOutline2.append(xShelfOutline[j]*np.sin(theta) + yShelfOutline[j]*np.cos(theta)+ty+lineY[0])
 
 
 
     # for j in range(len(angleList)):
-    #     xFinal2.append((minLengthList[j] * np.cos(angleList[j]+rotY[i]) + lineX[i])*1)
-    #     yFinal2.append((minLengthList[j] * np.sin(angleList[j]+rotY[i]) + lineY[i])*1)
-    xFinal2.append(lineX[i])
-    yFinal2.append(lineY[i])
-    # print(lineLength([lineX[i],lineY[i]],[xFinal2[-2],yFinal2[-2]]))
-    line.set_data(xFinal2, yFinal2)
+    #     xShelfOutline2.append((minLengthList[j] * np.cos(angleList[j]+rotY[i]) + lineX[i])*1)
+    #     yShelfOutline2.append((minLengthList[j] * np.sin(angleList[j]+rotY[i]) + lineY[i])*1)
+    xShelfOutline2.append(lineX[i])
+    yShelfOutline2.append(lineY[i])
+    # print(lineLength([lineX[i],lineY[i]],[xShelfOutline2[-2],yShelfOutline2[-2]]))
+    line.set_data(xShelfOutline2, yShelfOutline2)
     return line,
 
 
 
 def animate2(i):
-    xFinal2 = []
-    yFinal2 = []
+    xShelfOutline2 = []
+    yShelfOutline2 = []
     #for each point in the line, redraw the whole shelf
     
     for j in range(len(angleList)):
-        xFinal2.append((minLengthList[j] * np.cos(angleList[j]+rotY[i]) + lineX[i])*1)
-        yFinal2.append((minLengthList[j] * np.sin(angleList[j]+rotY[i]) + lineY[i])*1)
-    xFinal2.append(lineX[i])
-    yFinal2.append(lineY[i])
-    # xFinal2.append(lineX[i] + minLengthList[i] * np.cos(angleList[i]))
-    # yFinal2.append(lineY[i] + minLengthList[i] * np.sin(angleList[i]))
+        xShelfOutline2.append((minLengthList[j] * np.cos(angleList[j]+rotY[i]) + lineX[i])*1)
+        yShelfOutline2.append((minLengthList[j] * np.sin(angleList[j]+rotY[i]) + lineY[i])*1)
+    xShelfOutline2.append(lineX[i])
+    yShelfOutline2.append(lineY[i])
+    # xShelfOutline2.append(lineX[i] + minLengthList[i] * np.cos(angleList[i]))
+    # yShelfOutline2.append(lineY[i] + minLengthList[i] * np.sin(angleList[i]))
 
-    line.set_data(xFinal2, yFinal2)
+    line.set_data(xShelfOutline2, yShelfOutline2)
     return line,
 
 anim = FuncAnimation(fig, animate, init_func=init,
@@ -272,6 +287,8 @@ anim = FuncAnimation(fig, animate, init_func=init,
 #                                frames=numAngles, interval=4000/numAngles, blit=True)
 
 # anim.save('rotating_shelf.gif', writer='Pillow',fps=60)
-
 plt.axis('equal')
+# plt.ylim([-6,5])
+# plt.xlim([-2,9])
+
 plt.show()